37882eb4f62d579aef1c4c47de89e487927d85b8
Anton <=>
2025-11-10 23:12:23 +0300

    avienRemoved

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 
 require('dotenv').config({ debug: false });
-
 console.log('DB_DATABASE:', process.env.DB_DATABASE);
 
 const express = require('express');
@@ -11,31 +10,8 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 app.set('trust proxy', 1);
 
-//avien
-// Aiven SSL gerektirir → ssl objesi ekle
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true // Aiven için zorunlu
-  }
-});
-
-app.get('/api/health', async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT 1 + 1 AS result');
-    res.json({ status: 'OK', db: rows[0].result });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`API running on port ${PORT}`);
-});
-
-// avien bitiş: const PORT = process.env.PORT || 3000;
 
 // ---------- GÜVENLİ IP ALIMI (IPv6 → IPv4) ----------
 const getCleanIp = (req) => {
@@ -139,3 +115,4 @@ app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
   console.log(`Türkiye saatiyle: ${new Date().toLocaleString('tr-TR', { timeZone: 'Europe/Istanbul' })}`);
 });
+
